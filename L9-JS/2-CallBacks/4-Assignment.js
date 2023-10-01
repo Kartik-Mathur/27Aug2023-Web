81b4663b867ad3e78e112a581d44ad2812d197e5
Kartik <kartik.mathur10@gmail.com>
2023-10-01 12:02:02 +0530

    Callbacks

--- L9-JS/2-CallBacks/4-Assignment.js
+++ L9-JS/2-CallBacks/4-Assignment.js
@@ -1,20 +1,40 @@
 // Download will take 3 seconds
-function download(){
-    
+function download(url, cb) {
+    // console.log(url);
+    console.log("Download Starts");
+    setTimeout(() => {
+        let fileName = url.split('/').pop();
+        // console.log(fileName);
+        console.log("Download Complete", fileName);
+        cb(fileName, upload);
+    }, 3000);
+
 }
 // Compression will take 3 seconds
-function compress(){
+function compress(fileName, cb) {
     // File.mp4 -->  file.zip
+    console.log("Compression Starts");
+    setTimeout(() => {
+        let compressedFile = fileName.split('.')[0] + '.zip';
+        console.log("Compression Complete",compressedFile);
+        cb(compressedFile,allDone);
+    }, 3000);
 }
 
 // Upload will take 3 seconds
-function upload(){
+function upload(compressedFile, cb) {
     // new url should be http://newcb.com/file.zip
+    console.log("Uploading starts");
+    setTimeout(()=>{
+        let newUrl = 'http://newcb.com/'+compressedFile;
+        console.log("Uploading Complete",newUrl);
+        cb(newUrl);
+    },3000);
 }
 
-function allDone(newUrl){
-    console.log("All done",newUrl);
+function allDone(newUrl) {
+    console.log("All done", newUrl);
 }
 
 let url = 'http://cb.com/file.mp4';
-download(url);
+download(url, compress);
