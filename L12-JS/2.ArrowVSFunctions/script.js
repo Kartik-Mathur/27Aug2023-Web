e5bda5061e207fcf2964dd0cf39c359ae22bea2e
Kartik <kartik.mathur10@gmail.com>
2023-10-14 11:46:22 +0530

    ArrowFunctions and Normal Functions

---
+++ L12-JS/2.ArrowVSFunctions/script.js
@@ -0,0 +1,26 @@
+function hello(){
+    console.log(this);
+    console.log("hello");
+}
+let world = ()=>{
+    console.log(this);
+    console.log("World");
+}
+
+let obj = {
+    a: 1,
+    b: true
+}
+let obj1 = {
+    a: "Hello",
+    b: "World"
+}
+
+hello.call(obj);// this-->obj
+hello.call(obj1);// this--> obj1
+world.call(obj);
+world.call(obj1);
+
+// hello();
+// world();
+
