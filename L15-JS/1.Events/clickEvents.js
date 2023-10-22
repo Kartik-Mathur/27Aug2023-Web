82ebdcecf21f77202836e12484193c3c39daebb5
Kartik <kartik.mathur10@gmail.com>
2023-10-22 11:14:56 +0530

    Events

--- L15-JS/1.Events/clickEvents.js
+++ L15-JS/1.Events/clickEvents.js
@@ -1,5 +1,6 @@
 let btn = document.querySelector('#btn');
 let body = document.querySelector('body');
+let nav = document.querySelector('.navbar');
 
 console.log(btn);
 
@@ -11,15 +12,24 @@ btn.addEventListener('click',()=>{
     console.log("Hello");
 });
 
-body.addEventListener('keydown',(ev)=>{
-    // console.log("You pressed a key");
-    console.log(ev.key);
+body.addEventListener('keydown',(x)=>{
+    console.log("You pressed a key");
+    console.log(x);
 });
 
 btn.addEventListener('mouseenter',()=>{
     console.log("You are hovering on me");
 })
 
-body.addEventListener('scroll',(ev)=>{
-    console.log(ev);
+document.addEventListener('scroll',(ev)=>{
+    // console.log(ev);
+    if(window.scrollY>765){
+        nav.classList.add('orange');
+        nav.classList.remove('white');
+    }
+    else{
+        nav.classList.add('white');
+        nav.classList.remove('orange');
+    }
 })
+
