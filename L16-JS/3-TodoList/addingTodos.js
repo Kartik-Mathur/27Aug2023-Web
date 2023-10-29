35dce0b1a793b0f3bcfa32f833feac1a650a54a9
Kartik <kartik.mathur10@gmail.com>
2023-10-29 11:35:03 +0530

    JQUERY

--- L16-JS/3-TodoList/addingTodos.js
+++ L16-JS/3-TodoList/addingTodos.js
@@ -2,7 +2,7 @@ let input = document.querySelector('.inp');
 let addTask = document.querySelector('.btn');
 let taskList = document.querySelector('.factsList');
 
-addTask.addEventListener('click',(ev)=>{
+let newTaskAdd = ()=>{
     if(input.value.length == 0) return;
     let div = document.createElement('div');
     div.innerHTML = `
@@ -19,5 +19,14 @@ addTask.addEventListener('click',(ev)=>{
     div.classList.add('factItemParent')
     input.value = '';
 
-    factsList.appendChild(div);
+    factsList.appendChild(div); 
+}
+
+addTask.addEventListener('click',(ev)=>{
+    newTaskAdd();
+})
+
+document.querySelector('body').addEventListener('keypress',(ev)=>{
+    if(ev.key!=='Enter') return;
+    newTaskAdd();
 })
