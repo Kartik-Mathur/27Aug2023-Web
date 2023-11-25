1b9a47f993008fdf532bed6e345b0f888824855f
Kartik <kartik.mathur10@gmail.com>
2023-11-25 13:01:21 +0530

    TodoApp

--- L20-Express/TodoApp/public/scripts/script.js
+++ L20-Express/TodoApp/public/scripts/script.js
@@ -1,1 +1,30 @@
-console.log("hello World")
+let btn = document.querySelector('#btn');
+let task = document.querySelector('#task');
+let taskList = document.querySelector('.taskList');
+
+function updateList(tasks){
+    taskList.innerHTML = '';
+    tasks.forEach((t)=>{
+        let li = document.createElement('li');
+        li.innerText = t;
+        taskList.appendChild(li);
+    })
+}
+
+function initialiseList(){
+    axios.get(`/gettasks`)
+    .then((data)=>{
+        data = data.data;
+        updateList(data);
+    })
+}
+
+initialiseList();
+
+btn.addEventListener('click',(ev)=>{
+    axios.get(`/addtask?task=${task.value}`)
+        .then((data)=>{
+            data = data.data;
+            updateList(data);
+        })
+})
