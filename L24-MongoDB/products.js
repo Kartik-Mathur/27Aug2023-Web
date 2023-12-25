eca9edfbcbe42fa0d094f957e5e9b7d4b5f1df51
Kartik <kartik.mathur10@gmail.com>
2023-12-25 12:16:22 +0530

    MongoDB

---
+++ L24-MongoDB/products.js
@@ -0,0 +1,17 @@
+[
+    {
+        name: 'Laptop',
+        price: 500,
+        description : 'Badhiya Laptop'
+    },
+    {
+        name: 'Mobile',
+        price: 5100,
+        description : 'Mast Mobile'
+    },
+    {
+        name: 'Shoe',
+        price: 1500,
+        description : 'Sexy Shoe'
+    }
+]
---
+++ L24-MongoDB/users.js
@@ -0,0 +1,10 @@
+[
+    {
+        name: 'Aryan',
+        orders: []
+    },
+    {
+        name: 'Joyeta',
+        orders: []
+    }
+]
